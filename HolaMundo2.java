b42e0218e674a0965f9e6ec80aab3d7cbee46b5f
cromerofajar <43774788+cromerofajar@users.noreply.github.com>
2018-10-11 09:32:09 +0200

    Add files via upload

---
+++ HolaMundo2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author cromerofajar
+ */
+public class HolaMundo2 {
+    public static void hg(String[] args) {
+        System.out.println("Mundo Mundo");
+    
+}
+}
